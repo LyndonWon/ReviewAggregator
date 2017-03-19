6f1d42d7f98482dae4448f9d8dac74e00a8bcbb7
Lyndon Won <lwon@shaw.ca>
2017-03-18 18:29:37 -0700

    added routes and api calls to search restaurants and get details of restaurants

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,51 @@
 var express = require('express');
+var request = require('request');
+var rp = require('request-promise');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/search', function(req, res, next) {
+  if (req.query.query) {
+    var query = req.query.query;
+    var googleSearchOptions = {
+      uri: 'https://maps.googleapis.com/maps/api/place/textsearch/json',
+      qs: {
+        query: query,
+        key: 'AIzaSyBHZ1lNnf4bGtcQSg9BVR3sIKnxCPrFQ28'
+      },
+      json: true
+    };
+    rp(googleSearchOptions)
+    .then(function(json) {
+      console.log(json);
+      res.render('search', {restaurants: json.results});
+    });
+  } else {
+    res.render('search');
+  }
+});
+
+router.get('/restaurant/:place_id', function(req, res, next) {
+  var place_id = req.params.place_id;
+  console.log(place_id);
+  var googleDetailOptions = {
+    uri: 'https://maps.googleapis.com/maps/api/place/details/json',
+    qs: {
+      place_id: place_id,
+      key: 'AIzaSyBHZ1lNnf4bGtcQSg9BVR3sIKnxCPrFQ28'
+    },
+    json: true
+  };
+  rp(googleDetailOptions)
+  .then(function(json) {
+    console.log(json);
+    res.render('restaurant', {restaurant: json.result});
+  });
+});
+
 
 module.exports = router;
